79f8d51f6d49549bbef92b653bcb19c2d22af8c5
Moto Sakanosita <moto@sakanosita.org>
2022-01-17 16:34:33 +0900

    chore(smacon-dev): update

--- src/templates/tags-template.js
+++ src/templates/tags-template.js
@@ -15,7 +15,7 @@ const TagsTemplate = ({ pageContext, data }) => {
     <Layout title={title}>
       <TagsTemplateWrapper>
         <Title>
-          {tag} development - {totalCount} pages
+          {tag} - {totalCount} pages
         </Title>
         <Link
           css={`

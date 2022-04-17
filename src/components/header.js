d47e104f9d0b6c29ba8ea12a286895d4716342c1
Moto Sakanosita <moto@sakanosita.org>
2022-04-17 20:33:20 +0900

    chore(smacon-dev): update

--- src/components/header.js
+++ src/components/header.js
@@ -27,15 +27,15 @@ const Header = () => {
         <HeaderNavList>
 
           <HeaderNavListItem>
-            <Link to="/solidity">Solidity</Link>
+            <Link to="/solidity/">Solidity</Link>
           </HeaderNavListItem>
 
           <HeaderNavListItem>
-            <Link to="/rust">Rust</Link>
+            <Link to="/rust/">Rust</Link>
           </HeaderNavListItem>
 
           <HeaderNavListItem>
-            <Link to="/motoko">Motoko</Link>
+            <Link to="/motoko/">Motoko</Link>
           </HeaderNavListItem>
 
           {/* <HeaderNavListItem>

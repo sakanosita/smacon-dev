af0413582586393ce0b648fbab37208dfc1b4e03
Moto Sakanosita <moto@sakanosita.org>
2022-04-24 21:57:55 +0900

    chore(smacon-dev): update

--- src/components/tags.js
+++ src/components/tags.js
@@ -2,13 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'gatsby';
 
-const toKebabCase = (str) => {
-  return str
-    .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
-    .map((x) => x.toLowerCase())
-    .join('-');
-};
-
 const Tags = ({ tags }) => {
   return (
     <TagsList>
--- src/pages/tags.js
+++ src/pages/tags.js
@@ -3,13 +3,6 @@ import Layout from '../components/layout';
 import { Link, graphql } from 'gatsby';
 import styled from 'styled-components';
 
-const toKebabCase = (str) => {
-  return str
-    .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
-    .map((x) => x.toLowerCase())
-    .join('-');
-};
-
 const Tags = ({ data }) => {
   const tags = data.allMarkdownRemark.group;
 

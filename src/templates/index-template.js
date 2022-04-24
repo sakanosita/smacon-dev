e9ec55dbcb3ce68ece1b499af3cec4a932e902c3
Moto Sakanosita <moto@sakanosita.org>
2022-04-24 12:17:50 +0900

    chore(smacon-dev): update

--- src/templates/index-template.js
+++ src/templates/index-template.js
@@ -33,19 +33,19 @@ const HomePage = ({ data }) => {
 
       <Board>
         <h3>
-          <Link to="/solidity/">Solidity入門</Link>
+          <Link to="/solidity/">Solidityプログラミング入門</Link>
         </h3>
         <PostList posts={solidityPosts} />
       </Board>
       <Board>
         <h3>
-          <Link to="/rust/">Rust入門</Link>
+          <Link to="/rust/">Rustプログラミング入門</Link>
         </h3>
         <PostList posts={rustPosts} />
       </Board>
       <Board>
         <h3>
-          <Link to="/motoko/">Motoko入門</Link>
+          <Link to="/motoko/">Motokoプログラミング入門</Link>
         </h3>
         <PostList posts={motokoPosts} />
       </Board>

86a2ae70b26ec97b24732f2cc909653820d2dea8
Moto Sakanosita <moto@sakanosita.org>
2022-04-27 00:21:22 +0900

    chore(smacon-dev): update

--- src/components/global-styles.js
+++ src/components/global-styles.js
@@ -91,8 +91,8 @@ body::-webkit-scrollbar-track {
 
 :root {
   scroll-behavior: smooth;
-  --size-300: 0.75rem;
-  --size-400: 1rem;
+  --size-300: 0.92rem;
+  --size-400: 1.1rem;
   --size-500: 1.33rem;
   --size-600: 1.77rem;
   --size-700: 2.03rem;

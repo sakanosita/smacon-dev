692ce48f85f2e7be898c2936406a3fbec41b1833
Moto Sakanosita <moto@sakanosita.org>
2022-04-27 09:55:58 +0900

    chore(smacon-dev): update

--- src/components/sharing-button-list.js
+++ src/components/sharing-button-list.js
@@ -7,48 +7,50 @@ import {
     LineShareButton,
     TwitterIcon,
     TwitterShareButton
-  } from 'react-share'
-  
-  import React from 'react'
-  import styled from '@emotion/styled'
-  
-  const Wrapper = styled.div`
-    display: flex;
-    padding-bottom: 24px;
-  `
-  
-  const ButtonWrapper = styled.div`
-    padding-right: 12px;
-  `
-  
-  const ShareButtonList = ({title, url}) => {
-    return (
-      <Wrapper>
-        <ButtonWrapper>
-          <FacebookShareButton url={url}>
-            <FacebookIcon size={36} round />
-          </FacebookShareButton>
-        </ButtonWrapper>
-  
-        <ButtonWrapper>
-          <TwitterShareButton title={title} url={url} >
-            <TwitterIcon size={36} round />
-          </TwitterShareButton>
-        </ButtonWrapper>
-  
-        <ButtonWrapper>
-          <LineShareButton url={url} >
-            <LineIcon size={36} round />
-          </LineShareButton>
-        </ButtonWrapper>
+} from 'react-share'
 
-        {/* <ButtonWrapper>
-          <HatenaShareButton url={url} >
-            <HatenaIcon size={40} round />
-          </HatenaShareButton>
-        </ButtonWrapper> */}
-      </Wrapper>
-    )
-  }
-  
-  export default ShareButtonList
+import React from 'react'
+import styled from '@emotion/styled'
+
+const ShareButtonList = ({title, url}) => {
+  return (
+    <Wrapper>
+      <ButtonWrapper>
+        <FacebookShareButton url={url}>
+          <FacebookIcon size={36} round />
+        </FacebookShareButton>
+      </ButtonWrapper>
+
+      <ButtonWrapper>
+        <TwitterShareButton title={title} url={url} >
+          <TwitterIcon size={36} round />
+        </TwitterShareButton>
+      </ButtonWrapper>
+
+      <ButtonWrapper>
+        <LineShareButton url={url} >
+          <LineIcon size={36} round />
+        </LineShareButton>
+      </ButtonWrapper>
+
+      {/* <ButtonWrapper>
+        <HatenaShareButton url={url} >
+          <HatenaIcon size={40} round />
+        </HatenaShareButton>
+      </ButtonWrapper> */}
+    </Wrapper>
+  )
+}
+
+export default ShareButtonList
+
+   
+const Wrapper = styled.div`
+  display: flex;
+  margin-left: var(--size-400);
+  padding-bottom: 24px;
+`
+
+const ButtonWrapper = styled.div`
+  padding-right: 12px;
+`

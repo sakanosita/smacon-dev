25d22de4eee3ab42d581a443e58eced776a1ce90
Moto Sakanosita <moto@sakanosita.org>
2022-04-27 10:08:59 +0900

    chore(smacon-dev): update

--- src/templates/tag-template.js
+++ src/templates/tag-template.js
@@ -4,24 +4,30 @@ import Layout from '../components/layout';
 import PostList from '../components/post-list';
 import ViewAllTags from '../components/view-all-tags';
 import styled from 'styled-components';
+import ShareButtonList from '../components/sharing-button-list';
 
 const TagsTemplate = ({ pageContext, data }) => {
+  const site = data.site;
   const { tag } = pageContext;
   // const { totalCount } = data.allMarkdownRemark;
   const posts = data.allMarkdownRemark.nodes;
-  const title = `${tag} | smacon.dev`;
+  const title = `はじめてのスマートコントラクト開発 – '${tag}' の検索結果`;
 
   return (
     <Layout title={title}>
+
       <TagsTemplateWrapper>
         <Title>
           '{tag}' の検索結果
         </Title>
-
         <PostList posts={posts} />
-
-        <ViewAllTags/>
       </TagsTemplateWrapper>
+
+      <ViewAllTags/>
+      <ShareButtonList
+          title={title}
+          url={`${site.siteMetadata.siteUrl}`}
+        />
     </Layout>
   );
 };
@@ -30,6 +36,8 @@ export default TagsTemplate;
 
 const TagsTemplateWrapper = styled.div`
   padding-top: var(--size-900);
+  margin-top: var(--size-600);
+  margin-bottom: var(--size-600);
 `;
 
 const Title = styled.h1`
@@ -38,6 +46,12 @@ const Title = styled.h1`
 
 export const pageQuery = graphql`
   query($tag: String) {
+    site {
+      siteMetadata {
+        title
+        siteUrl
+      }
+    }
     allMarkdownRemark(
       limit: 2000
       sort: { fields: [frontmatter___date], order: DESC }

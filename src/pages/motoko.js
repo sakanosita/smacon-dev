de1fa15e6ef243154baccbf40ea6388bc44cd728
Moto Sakanosita <moto@sakanosita.org>
2022-04-27 09:51:21 +0900

    chore(smacon-dev): update

--- src/pages/motoko.js
+++ src/pages/motoko.js
@@ -67,7 +67,8 @@ const Intro = styled.div`
 const Board = styled.div`
   display: flex;
   flex-direction: column;
-  margin-bottom: var(--size-900);
+  margin-top: var(--size-900);
+  margin-bottom: var(--size-600);
   & h3 {
     font-size: 120%;
     text-align: center;

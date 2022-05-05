56b76e30e850c56e328a7e471d67690a044162d6
Moto Sakanosita <moto@sakanosita.org>
2022-05-05 17:01:28 +0900

    chore(smacon-dev): update

--- src/templates/post-template.js
+++ src/templates/post-template.js
@@ -5,7 +5,6 @@ import RelatedList from '../components/related-list';
 import ShareButtonList from '../components/sharing-button-list';
 import styled from 'styled-components';
 import Tags from '../components/tags';
-import { getImage } from 'gatsby-plugin-image';
 
 const PostTemplate = ({ data }) => {
   const site = data.site;

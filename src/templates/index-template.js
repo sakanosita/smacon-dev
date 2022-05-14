69eba5e070d96f1daf44ecaff2bf337062962357
Moto Sakanosita <moto@sakanosita.org>
2022-05-15 08:17:09 +0900

    chore(smacon-dev): update

--- src/templates/index-template.js
+++ src/templates/index-template.js
@@ -100,7 +100,7 @@ const Board = styled.div`
   margin-top: var(--size-900);
   margin-bottom: var(--size-600);
   & h3 {
-    font-size: var(--size-400);
+    font-size: var(--size-500);
     text-align: center;
     & a {
       color: inherit;

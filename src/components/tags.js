050f4d5608e7499620c1ba909505e503b9130d6a
Moto Sakanosita <moto@sakanosita.org>
2022-05-16 20:43:15 +0900

    chore(smacon-dev): update

--- src/components/tags.js
+++ src/components/tags.js
@@ -34,7 +34,7 @@ const TagItem = styled.span`
   & a {
     position: relative;
     z-index: 2;
-    background-color: rgba(255, 255, 255, 0.5);
+    background-color: rgba(255, 255, 255, 0.6);
     text-decoration: none;
     color: inherit;
     padding: 0.2rem 0.6rem;

f0f4af508c36da00a68634355506604f0d7fc1ac
Moto Sakanosita <moto@sakanosita.org>
2022-05-17 23:06:15 +0900

    chore(smacon-dev): update

--- src/components/post-list.js
+++ src/components/post-list.js
@@ -89,11 +89,6 @@ const StyledPostListItem = styled.li`
     background-color: rgba(255, 255, 255, 0.5);
   }
 
-  @media screen and (max-width: 500px) {
-    & {
-      margin-top: var(--size-600);
-    }
-  }
 `;
 
 const PostListTitle = styled.h2`

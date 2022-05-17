cba8a8303cb9d48af7db356ba0b736182264868c
Moto Sakanosita <moto@sakanosita.org>
2022-05-17 23:59:23 +0900

    chore(smacon-dev): update

--- src/templates/post-template.js
+++ src/templates/post-template.js
@@ -114,6 +114,7 @@ const PostContent = styled.section`
     // font-family: 'Source Sans Pro', monospace;
     overflow-x: auto;
     white-space: pre-wrap;
+    font-size: 85%;
     @media screen and (max-width: 700px) {
       font-size: 85%;
     }

ab1cdbb0c13dbe5b8f1c065d10c69879097329d3
Moto Sakanosita <moto@sakanosita.org>
2022-05-19 22:46:20 +0900

    chore(smacon-dev): update

--- src/components/styled-link.js
+++ src/components/styled-link.js
@@ -2,15 +2,12 @@ import { Link } from 'gatsby';
 import styled from 'styled-components';
 
 const StyledLink = styled(Link)`
-  padding: 0.5rem;
-  padding-left: 1.5rem;
-  padding-right: 1.5rem;
+  padding: 0.8rem;
   color: inherit;
   background-color: rgba(255, 255, 255, 0.4);
   text-decoration: none;
   border-radius: 0px;
-  border: 1px solid rgba(255, 255, 255, 0.8);
-  text-transform: uppercase;
+  border: 1px solid rgba(255, 255, 255, 1);
   border-radius: 4px;
 `;
 
--- src/components/view-all-tags.js
+++ src/components/view-all-tags.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import StyledLink from '../components/styled-link';
+import styled from 'styled-components';
 
 const ViewAllTags = () => {
 
@@ -8,16 +9,24 @@ const ViewAllTags = () => {
         css={`
         margin-top: 1rem;
         margin-bottom: 3rem;
-        display: block;
         margin-left: auto;
         margin-right: auto;
+        display: flex;
+        font-weight: bold;
         width: fit-content;
+        opacity: 0.6;
         `}
         to="/tags/"
     >
-    キーワードを見る
+      <SearchIcon src="/media/search.png"></SearchIcon>
+      キーワード
     </StyledLink>
   );
 };
 
 export default ViewAllTags;
+
+const SearchIcon = styled.img`
+  margin-right: 0.4rem;
+  height: 1.4rem;
+`;

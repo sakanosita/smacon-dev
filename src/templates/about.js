76f7dc86cb359581c855dc525320ad3eea342491
Moto Sakanosita <moto@sakanosita.org>
2022-05-22 14:49:56 +0900

    chore(smacon-dev): update

--- src/templates/about.js
+++ src/templates/about.js
@@ -18,7 +18,7 @@ const AboutTemplate = ({ pageContext, data }) => {
       <BreadcrumbStyled>
         <Breadcrumb
           crumbs={crumbs}
-          crumbLabel='About Us'
+          crumbLabel='About us'
           crumbSeparator=' > '
         />
       </BreadcrumbStyled>

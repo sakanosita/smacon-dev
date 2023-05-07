45a5a240c01a629e01b320956634b82231c8d57b
Moto Fukao <fukao@nagamina.com>
2023-05-07 16:35:23 +0900

    Update

--- src/components/header.js
+++ src/components/header.js
@@ -18,10 +18,10 @@ const Header = () => {
       <HeaderNavWrapper>
         <HeaderNavList>
           <HeaderNavListItem>
-            <Link to="/motoko/">Motoko</Link>
+            <Link to="/solidity/">Solidity</Link>
           </HeaderNavListItem>
           <HeaderNavListItem>
-            <Link to="/solidity/">Solidity</Link>
+            <Link to="/motoko/">Motoko</Link>
           </HeaderNavListItem>
           <HeaderNavListItem>
             <Link to="/rust/">Rust</Link>
